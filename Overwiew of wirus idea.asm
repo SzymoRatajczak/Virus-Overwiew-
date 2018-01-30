df0c1d1f77cd3fef7749e5fb7698d658f99f3a45
Szymon Ratajczak <daniel.kruk1@interia.pl>
2018-01-30 01:28:42 +0100

    Add files via upload

---
+++ Overwiew of wirus idea.asm
@@ -0,0 +1,28 @@
+virus program 
+
+  sygnatura 1234567
+   subroutine infection_of_exe_file
+    begin
+      loop: take_random_ file
+	    if first_line_of_file==1234567
+		  then goto petla
+		 perform compression
+		 attach virus at the begining of the file
+    end
+	
+	subroutine destroy
+	 begin
+	 any type of acition you want
+	 end
+
+	 
+	subroutine launch_statement 
+	 begin 
+	   check  if launch statemnt is meet
+	 end
+	  
+	begin 
+	   infection_of_exe_file
+	   if launch_statement then destroy
+        desompression of file
+    end		
